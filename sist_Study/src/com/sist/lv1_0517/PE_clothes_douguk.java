11a817b5e37009db610b16f7e698448a475e0bd5
donguk1101 <zeroska12@naver.com>
2021-05-17 19:45:34 +0900

    체육복

---
+++ sist_Study/src/com/sist/lv1_0517/PE_clothes_douguk.java
@@ -0,0 +1,24 @@
+package com.sist.lv1_0517;
+
+public class PE_clothes_douguk {
+	public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        answer=n-lost.length;
+        int num=0;
+        for(int i=0; i<reserve.length; i++){ 
+            for(int j=num; j<lost.length; j++){
+                
+                if(Math.abs(reserve[i]-lost[j])==1){
+                    answer++;
+                    i++;
+                    num++;
+                    if(i==reserve.length)
+                        break;
+                }    
+                
+            }
+            
+        }
+        return answer;
+    }
+}
