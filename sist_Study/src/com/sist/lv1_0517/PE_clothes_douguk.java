0ae3bfd66f2f85a76e00780bc659f9512130a2aa
donguk <73410931+donguk1101@users.noreply.github.com>
2021-05-18 06:57:48 +0900

    Update PE_clothes_douguk.java

--- sist_Study/src/com/sist/lv1_0517/PE_clothes_douguk.java
+++ sist_Study/src/com/sist/lv1_0517/PE_clothes_douguk.java
@@ -5,10 +5,18 @@ public class PE_clothes_douguk {
         int answer = 0;
         answer=n-lost.length;
         int num=0;
+        for(int k=0 k<reserve.length; k++){
+            for(int m=0; m<lost.length; m++){
+               if(reserve[i]==lost[j]){
+                  reserve[i]=0;
+                  lost[j]=0;
+                  }
+             }
+         }
         for(int i=0; i<reserve.length; i++){ 
             for(int j=num; j<lost.length; j++){
                 
-                if(Math.abs(reserve[i]-lost[j])<=1){
+                if(Math.abs(reserve[i]-lost[j])==1){
                     answer++;
                     i++;
                     num++;
