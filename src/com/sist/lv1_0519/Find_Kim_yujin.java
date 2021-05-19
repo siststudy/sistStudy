d24131b1282547adf95af1079238d681b192fadb
Youjin Kwon <iugean1208@gmail.com>
2021-05-19 21:27:08 +0900

    find_kim: yujin

---
+++ src/com/sist/lv1_0519/Find_Kim_yujin.java
@@ -0,0 +1,22 @@
+package com.sist.lv1_0519;
+
+
+
+public class Find_Kim_yujin {
+	public String solution(String[] seoul) {
+        String answer = "";
+        for(int i=0; i<seoul.length; i++)
+        {
+            if(seoul[i].equals("Kim"))
+            {
+                answer="김서방은 "+i+"에 있다";
+                break;
+            }
+                
+        }
+        return answer;
+    }
+ 
+ 
+
+}
