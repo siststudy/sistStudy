4ed0e16a1e20b273c99152991645c35b256806cf
user <user@HONGYJ>
2021-05-20 22:05:44 +0900

    홍유정

--- src/com/sist/lv1_0520/crain_doll_hong.java
+++ src/com/sist/lv1_0520/crain_doll_hong.java
@@ -7,16 +7,16 @@ public class crain_doll_hong {
 	        int answer = 0;
 	        Stack<Integer> stack=new Stack<Integer>();
 	        
-	        for(int move:moves){
+	        for(int m:moves){
 	            for(int i=0;i<board.length;i++){
-	                if(board[i][move-1]!=0){
-	                    if(!stack.empty()&&stack.peek()==board[i][move-1]){
+	                if(board[i][m-1]!=0){
+	                    if(!stack.empty()&&stack.peek()==board[i][m-1]){
 	                        stack.pop();
 	                        answer+=2;
 	                    }else{
-	                        stack.push(board[i][move-1]);
+	                        stack.push(board[i][m-1]);
 	                    }
-	                    board[i][move-1]=0;
+	                    board[i][m-1]=0;
 	                    break;
 	                }
 	            }
