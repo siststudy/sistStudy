8720f0d98074e6bf9067e7dee6c3fe3114309b7d
Youjin Kwon <iugean1208@gmail.com>
2021-05-22 16:58:29 +0900

    yujin

---
+++ src/com/sist/lv1_0522/New_id_yujin.java
@@ -0,0 +1,52 @@
+package com.sist.lv1_0522;
+
+public class New_id_yujin {
+	/*
+	 * 파이썬 코드
+	import re
+
+	new_id="=.="
+
+
+	def solution(new_id):
+	    answer = ''
+	    #1단계
+	    new_id=new_id.lower()
+	    #2단계
+	    new_id=re.sub('[~!@#$%^&*\(\)=+\[\{\]\}:?,\<\>/]', '', new_id)
+	        
+	    #print(new_id)
+	    #3단계
+	    new_id=re.sub('\.+', '.', new_id)
+	    
+	    #4단계
+	    if len(new_id) > 0 :
+	        if new_id[0]==".":
+	            new_id=new_id[1:]
+	    if len(new_id) > 0 :  
+	        if new_id[-1]==".":
+	            new_id=new_id[:-1]
+	    
+	    #5단계
+	    if len(new_id) == 0:
+	            new_id="a"
+	    
+	    #6단계
+	    if len(new_id) > 15:
+	        new_id=new_id[:15]
+	        if new_id[-1] ==".":
+	            new_id=new_id[:-1]
+	    
+	    #7단계
+	    while len(new_id) < 3:
+	        new_id+=new_id[-1]
+	        
+	                     
+	            
+	    print(new_id,end="")
+	    answer=new_id
+	    return answer
+
+	solution(new_id)
+	*/
+}
