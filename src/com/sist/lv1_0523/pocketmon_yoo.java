b23f370428da241b7ac7d56357fe5ad6763afca8
rachel5004 <j26597719@naver.com>
2021-05-23 22:34:02 +0900

    ternary operation

--- src/com/sist/lv1_0523/pocketmon_yoo.java
+++ src/com/sist/lv1_0523/pocketmon_yoo.java
@@ -5,8 +5,7 @@ public class pocketmon_yoo {
 		public int solution(int[] nums) {
 	        Set<Integer> set = new HashSet<>();
 	        for(int i:nums) set.add(i);
-	        if(nums.length/2<=set.size()) return nums.length/2;
-	        return set.size();
+	        return nums.length/2<=set.size()?nums.length/2:set.size();
 	    }
 	}
 }
