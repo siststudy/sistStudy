2f9867316294ea4438e44ca67a1412e6ae8af280
Junho <u_innovation@naver.com>
2021-05-24 20:59:26 +0900

    3진법 뒤집기_프로그래머스

---
+++ src/com/sist/lv1_0524/ConverToThree_jun.java
@@ -0,0 +1,20 @@
+package com.sist.lv1_0524;
+
+public class ConverToThree_jun {
+	public int solution(int n) {
+        String answer = "";
+        // 3진법으로 변환
+        while (n > 0) {
+        	answer = answer + (n % 3); // 앞뒤 반전
+        	n /= 3;
+        }
+        System.out.println(answer);
+        return Integer.parseInt(answer, 3); // 10진법으로 변환
+        
+    }
+	
+	public static void main(String[] args) {
+		Jinbub_junho jin = new Jinbub_junho();
+		System.out.println(jin.solution(125));
+	}
+}
