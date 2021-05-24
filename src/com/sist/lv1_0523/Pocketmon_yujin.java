edc1f4bcdc667bc15a106dcae6af9bb4870516b3
Youjin Kwon <iugean1208@gmail.com>
2021-05-24 17:40:37 +0900

    yujin

---
+++ src/com/sist/lv1_0523/Pocketmon_yujin.java
@@ -0,0 +1,32 @@
+package com.sist.lv1_0523;
+
+public class Pocketmon_yujin {
+
+	/*
+	 * 	파이썬 코드
+	 * 	def solution(nums):
+		    #answer = 0
+		    list=[]
+		    list.append(nums[0])
+		    for i in range(1, len(nums)): 
+		        if not nums[i] in list:
+		            list.append(nums[i])
+		    
+		    #print(list)
+		    
+		    temp=[]
+		    if len(list) > len(nums)//2:
+		        for i in range(0, len(nums)//2):
+		            if not list[i] in temp:
+		                temp.append(list[i])
+		                answer=len(temp)
+		                if len(temp) >= len(nums)//2:
+		                    break
+		    else:
+		        answer=len(list)
+		    
+		    print(answer)
+		    return answer
+	 * 
+	 */
+}
