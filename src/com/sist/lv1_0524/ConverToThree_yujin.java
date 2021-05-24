0f3a12aafdb0065bccb3437be5db7a379bf7cf81
Youjin Kwon <iugean1208@gmail.com>
2021-05-24 23:17:36 +0900

    yujin

---
+++ src/com/sist/lv1_0524/ConverToThree_yujin.java
@@ -0,0 +1,19 @@
+package com.sist.lv1_0524;
+
+public class ConverToThree_yujin {
+	/*
+	 * 	파이썬 코드
+	 * 	def solution(n):
+		    answer=''
+		    
+		    while n>0:
+		        n,remain=divmod(n, 3)
+		        answer+=str(remain)
+		        
+		   
+		    answer=int(answer,3)
+		    print(answer)
+		    return(answer)    
+		solution(n)
+	 */
+}
