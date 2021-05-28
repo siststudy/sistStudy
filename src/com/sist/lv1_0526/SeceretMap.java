dc626e8632761a2418a8abbfdf80cebda6cb1899
rachel5004 <j26597719@naver.com>
2021-05-28 14:17:47 +0900

    비밀지도

---
+++ src/com/sist/lv1_0526/SeceretMap.java
@@ -0,0 +1,15 @@
+package com.sist.lv1_0526;
+
+import java.util.Arrays;
+
+public class SeceretMap {
+	public String[] solution(int n, int[] arr1, int[] arr2) {   
+        String[] answer = new String[n];
+        
+        for(int i=0;i<n;i++) {
+        	answer[i]=String.format("%"+n+"s",Integer.toBinaryString(arr1[i]|arr2[i]))
+        				.replaceAll("1", "#").replaceAll("0", " ");
+        }
+        return answer;
+    }
+}
--- src/com/sist/lv1_0526/delete.java
+++
@@ -1,5 +0,0 @@
-package com.sist.lv1_0526;
-
-public class delete {
-
-}
